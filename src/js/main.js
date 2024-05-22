9cf9e8cf3e72a7fd7275b8a74eb591169cb1daa9
Jakub Bilski <jakub.bilski@apsl.edu.pl>
2024-05-22 11:08:31 +0200

    Refactored toggle script.

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,5 @@
 const toggleMenuEl = document.getElementById('js-toggle-menu');
-
-if (toggleMenuEl) {
+toggleMenuEl?.addEventListener('click', () => {
     const menuEl = document.getElementById('js-menu');
-    if (menuEl) {
-        toggleMenuEl.addEventListener('click', () => {
-            if (menuEl.classList.contains('menu__nav-list--active')) {
-                menuEl.classList.remove('menu__nav-list--active');
-            } else {
-                menuEl.classList.add('menu__nav-list--active');
-            }
-        });
-    }
-}
+    menuEl?.classList.toggle('menu__nav-list--active');
+});
